fb05d01d5fe3530975ff4fd2252959cfa448aafa
Ivan <1095036273@qq.com>
2020-11-10 22:56:11 +0800

    忽略lib文件

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare class OssSrc {
-    private url;
-    constructor(url: string);
-    private init;
-    format(type: 'jpg' | 'png' | 'webp' | 'bmp' | 'gif' | 'tiff'): this;
-    getUrl(): String;
-}
-export default OssSrc;
