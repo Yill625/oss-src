871ac2ad776502ecfd99b26737d0843960a9e6ce
Ivan <1095036273@qq.com>
2020-11-11 22:33:56 +0800

    添加图片旋转图片缩放功能

--- src/__tests__/OssSrc.test.ts
+++ src/__tests__/OssSrc.test.ts
@@ -1,6 +1,19 @@
 import OssSrc from '../index';
-test('test format', () => {
+test('format', () => {
     expect(new OssSrc('https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg').format('webp').getUrl()).toBe(
         'https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/format,webp',
+    );
+});
+test('rotate', () => {
+    expect(new OssSrc('https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg').rotate(90).getUrl()).toBe(
+        'https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/rotate,90',
+    );
+});
+
+test('resize', () => {
+    expect(
+        new OssSrc('https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg').resize('fixed', 100, 100).getUrl(),
+    ).toBe(
+        'https://image-demo.oss-cn-hangzhou.aliyuncs.com/example.jpg?x-oss-process=image/resize,m_fixed,h_100,w_100',
     );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// https://help.aliyun.com/document_detail/183902.html?spm=a2c4g.11174283.6.731.2e5a7da2XS98Z9
 class OssSrc {
     private url: string;
 
@@ -11,14 +12,30 @@ class OssSrc {
         return this;
     }
     /*
-     *
-     * format()：设置图片格式 建议放在最后处理
+     * 设置图片格式 建议放在最后处理
+     * https://help.aliyun.com/document_detail/44703.html
      */
     format(type: 'jpg' | 'png' | 'webp' | 'bmp' | 'gif' | 'tiff') {
         this.url += `/format,${type}`;
         return this;
     }
-
+    /**
+     * @param {number}  value 旋转角度
+     *
+     * 通过旋转参数，将存储在OSS内的原图按指定方向旋转
+     * https://help.aliyun.com/document_detail/44690.html
+     */
+    rotate(value: number) {
+        this.url += `/rotate,${value}`;
+        return this;
+    }
+    /**图片缩放
+     * @param {}
+     */
+    resize(m: 'lfit' | 'mfit' | 'fill' | 'pad' | 'fixed' = 'lfit', w: number, h: number) {
+        this.url += `/resize,m_${m},h_${h},w_${w}`;
+        return this;
+    }
     getUrl() {
         return this.url;
     }
